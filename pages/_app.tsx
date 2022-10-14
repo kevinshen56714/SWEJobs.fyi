5710a522c01fa048b7d92cc83bbfb16753c03ed4
kevinshen56714 <kshen64@gatech.edu>
2022-10-14 11:36:48 -0400

    Add dev data and navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+const NavBar = () => {
+  return (
+    <div>
+      <div className="max-w-5xl mx-auto pt-10 sm:px-12 lg:px-5 px-4">nav</div>
+    </div>
+  )
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="max-w-5xl mx-auto pt-10 sm:px-12 lg:px-5 px-4">nav</div>
+      <NavBar />
       <div className="max-w-5xl mx-auto py-10 sm:px-12 lg:px-5 px-4">
         <Component {...pageProps} />
       </div>
