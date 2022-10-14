7e359c8b9e4e0ad35d59ff36d09a5be59351bba2
kevinshen56714 <kshen64@gatech.edu>
2022-10-14 01:02:05 -0400

    Better table layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <div className="container mx-auto px-4">nav</div>
-      <div className="container mx-auto px-4">
+      <div className="max-w-5xl mx-auto pt-10 sm:px-12 lg:px-5 px-4">nav</div>
+      <div className="max-w-5xl mx-auto py-10 sm:px-12 lg:px-5 px-4">
         <Component {...pageProps} />
       </div>
     </>
