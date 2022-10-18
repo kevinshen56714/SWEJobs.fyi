eba83c0e38491508cc55282fca47debbbd285925
Cameron Luo <cameronluo@gatech.edu>
2022-10-18 14:46:03 -0700

    Add Temporary Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,74 @@ import { useEffect } from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-      <div className="mx-auto max-w-5xl px-4 pt-10 sm:px-12 lg:px-5">nav</div>
+    <div className="sticky top-0 rounded border-gray-200 bg-white px-2 py-2.5 shadow-md dark:bg-gray-900 sm:px-4">
+      <div className="container mx-auto flex flex-wrap items-center justify-between">
+        <a href="https://swe-jobs-fyi.vercel.app" className="flex items-center">
+          <img src="/global-job.svg" className="mr-3 h-6 sm:h-9" alt="swe-jobs logo" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            SWEJobs.fyi
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="h-6 w-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900">
+            <li>
+              <a
+                href="#"
+                className="block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
+                aria-current="page"
+              >
+                Jobs
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+              >
+                Trends
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block rounded py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
