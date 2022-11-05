8efae7a8a21f4035535ac7739b0543fdeaa7da83
kevinshen56714 <kshen64@gatech.edu>
2022-11-04 23:21:25 -0400

    Test more

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,16 +9,20 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta property="og:locale" content="en_US" />
         <meta property="og:site_name" content="SWEJobs.fyi" />
         <meta property="og:url" content="https://swejobs.fyi/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="http://swejobs.fyi/main-schematic.png" />
+        <meta property="og:image" content="https://swejobs.fyi/main-schematic.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:image:secure_url" content="https://swejobs.fyi/main-schematic.png" />
+        <meta property="article:modified_time" content="2022-09-28T19:38:02+00:00" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="swejobs.fyi" />
         <meta property="twitter:url" content="https://swejobs.fyi/" />
-        <meta name="twitter:image" content="https://swejobs.fyi/main-schematic.jpg" />
+        <meta name="twitter:image" content="https://swejobs.fyi/main-schematic.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
