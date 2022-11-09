0b0271874e8643bd1414757db1804217261f74d4
kevinshen56714 <kshen64@gatech.edu>
2022-11-08 22:58:21 -0500

    Fix a skill filtering bug

--- pages/jobs/[city]/[slug].tsx
+++ pages/jobs/[city]/[slug].tsx
@@ -87,11 +87,13 @@ export default function JobPosts(props: { jobs: Job[] }) {
   console.log('job post rerendered')
 
   const handleSkillBadgeClick = (skill: string) => {
-    const skillsStr = [...filter, skill].reduce(
-      (acc, cur, i) => acc + `${i === 0 ? '?' : '&'}skills=${encodeURIComponent(cur)}`,
-      ''
-    )
-    router.push(`/jobs/${city}/${slug}${skillsStr}`)
+    if (!filter.includes(skill)) {
+      const skillsStr = [...filter, skill].reduce(
+        (acc, cur, i) => acc + `${i === 0 ? '?' : '&'}skills=${encodeURIComponent(cur)}`,
+        ''
+      )
+      router.push(`/jobs/${city}/${slug}${skillsStr}`)
+    }
   }
 
   const handleCancelFilter = (skill: string) => {
