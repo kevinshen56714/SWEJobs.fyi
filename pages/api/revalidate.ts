11e74292e3fa309a51b4d2f37094d85d97d8b9ab
kevinshen56714 <kshen64@gatech.edu>
2022-11-11 11:55:12 -0500

    Also revalidate the index page

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -26,6 +26,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       ...Object.keys(SkillType).map(
         async (skillType) => await res.revalidate(`/trends/${city}/${skillType}/weekly-trends`)
       ),
+      // Home page
+      await res.revalidate('/'),
     ])
     return res.json({ revalidated: true })
   } catch (err) {
